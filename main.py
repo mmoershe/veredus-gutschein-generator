45d3bde032c93d08ad5dc64dc8f8d338b58ef912
mmoershe <92526921+mmoershe@users.noreply.github.com>
2024-11-23 21:18:46 +0100

    adding .env and copy to second output

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from modules.setup import setup
 from modules.code import generate_code
 from modules.tkinter import ask_for_price_gui
 from modules.generation import generate_gift_card
-
+from modules.CONST import OUTPUT_FILE_DIR
 
 from datetime import datetime
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 
 if __name__ == "__main__": 
@@ -13,3 +16,11 @@ if __name__ == "__main__":
     code: str = generate_code()
     date_text: str = datetime.today().strftime('%d-%m-%Y')
     generate_gift_card(price, code, date_text)
+    
+    SECONDARY_OUTPUT_DIR: str | None = os.getenv("SECONDARY_OUTPUT_DIR")
+    if SECONDARY_OUTPUT_DIR: 
+        import shutil
+        try:
+            shutil.copy(OUTPUT_FILE_DIR, SECONDARY_OUTPUT_DIR)
+        except: 
+            print("Failed to copy output to secondary_output")
